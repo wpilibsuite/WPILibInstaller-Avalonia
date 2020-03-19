5a7ea7b475d959bfdaad3b42f63ca9c5ff9a2120
Thad House <thadhouse1@gmail.com>
2020-03-19 14:13:09 -0700

    Working on a C++ shortcut creator

---
+++ WindowsShortcutCreator/src/main/cpp/ShortcutCreator.cpp
@@ -0,0 +1,79 @@
+#include "ShortcutCreator.h"
+
+#include <optional>
+
+
+#include "shobjidl.h"
+#include "objidl.h"
+#include "shlguid.h"
+#include "Shlobj.h"
+#include "Shlobj_core.h"
+#include "ComPtr.h"
+
+
+#pragma comment(lib, "Shell32.lib")
+
+ShortcutCreator::ShortcutCreator() {
+  m_hr = CoGetClassObject(CLSID_ShellLink, CLSCTX_INPROC_SERVER, NULL, IID_PPV_ARGS(shellLinkFactory.GetAddressOf()));
+}
+
+bool ShortcutCreator::CreateShortcut(std::wstring path, std::wstring destination) const {
+  HRESULT hres;
+  com::ComPtr<IShellLink> shellLink;
+
+  hres = shellLinkFactory->CreateInstance(NULL, IID_PPV_ARGS(shellLink.GetAddressOf()));
+
+  if (FAILED(hres)) return false;
+
+  auto persistFile = shellLink.As<IPersistFile>();
+
+  if (!persistFile) return false;
+
+  shellLink->SetPath(path.c_str());
+
+  hres = persistFile->Save(destination.c_str(), true);
+
+  return SUCCEEDED(hres);
+}
+
+std::optional<std::wstring> ShortcutCreator::GetLocalStartMenuFolder() const {
+  PWSTR path;
+  if (SUCCEEDED(SHGetKnownFolderPath(FOLDERID_StartMenu, 0, NULL, &path))) {
+    std::wstring retVal{path};
+    CoTaskMemFree(path);
+    return retVal;
+  }
+  return nullptr;
+}
+
+std::optional<std::wstring> ShortcutCreator::GetAllUsersStartMenuFolder() const {
+  PWSTR path;
+  if (SUCCEEDED(SHGetKnownFolderPath(FOLDERID_CommonStartMenu, 0, NULL, &path))) {
+    std::wstring retVal{path};
+    CoTaskMemFree(path);
+    return retVal;
+  }
+  return nullptr;
+}
+
+std::optional<std::wstring> ShortcutCreator::GetPublicDesktopFolder() const {
+  PWSTR path;
+  if (SUCCEEDED(SHGetKnownFolderPath(FOLDERID_PublicDesktop, 0, NULL, &path))) {
+    std::wstring retVal{path};
+    CoTaskMemFree(path);
+    return retVal;
+  }
+  return nullptr;
+}
+
+// bool ShortcutCreator::Create() const {
+//   HRESULT hres;
+//   com::ComPtr<IShellLink> shellLink;
+
+//   hres = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(shellLink.GetAddressOf()));
+
+//   if (FAILED(hres)) return false;
+
+
+//   auto persistFile = shellLink.As<IPersistFile>();
+// }
---
+++ WindowsShortcutCreator/src/main/headers/ShortcutCreator.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <optional>
+
+#include "shobjidl.h"
+#include "objidl.h"
+#include "shlguid.h"
+#include "ComPtr.h"
+
+class ShortcutCreator {
+public:
+  ShortcutCreator();
+
+
+  bool CreateShortcut(std::wstring path, std::wstring destination) const;
+  bool CreateFolder() const;
+
+  std::optional<std::wstring> GetLocalStartMenuFolder() const;
+  std::optional<std::wstring> GetAllUsersStartMenuFolder() const;
+
+    std::optional<std::wstring> GetPublicDesktopFolder() const;
+
+   operator HRESULT() const { return m_hr; }
+
+
+private:
+  com::ComPtr<IClassFactory> shellLinkFactory;
+   HRESULT m_hr;
+};
