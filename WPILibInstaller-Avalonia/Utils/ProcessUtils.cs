e49a9dc67da69e507ce154baf5cd1fd5471a45d9
Thad House <ThadHouse@users.noreply.github.com>
2020-09-25 17:52:20 -0700

    Create ISO from built files (#18)
    * Create ISO from built files
    
    * formatting
    
    * Fix executable path

--- WPILibInstaller-Avalonia/Utils/ProcessUtils.cs
+++ WPILibInstaller-Avalonia/Utils/ProcessUtils.cs
@@ -7,17 +7,7 @@ namespace WPILibInstaller_Avalonia.Utils
     {
         public static string GetStartingExecutablePath()
         {
-            string executablePath = System.Diagnostics.Process.GetCurrentProcess().MainModule!.FileName!;
-            string executable = System.IO.Path.GetFileNameWithoutExtension(executablePath);
-
-            if ("dotnet".Equals(executable, StringComparison.InvariantCultureIgnoreCase))
-            {
-                return typeof(ProcessUtils).Assembly.Location;
-            }
-            else
-            {
-                return executablePath;
-            }
+            return System.AppContext.BaseDirectory;
         }
     }
 }
