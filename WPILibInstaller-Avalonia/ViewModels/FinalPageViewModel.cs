26b4016133027d0ea2b0745d57ee87848a15c664
Thad House <ThadHouse@users.noreply.github.com>
2020-10-29 17:55:14 -0700

    Make finish page properly handle vscode settings and popup (#51)
    * Make finish page properly handle vscode settings and popup
    
    * A review comment

--- WPILibInstaller-Avalonia/ViewModels/FinalPageViewModel.cs
+++ WPILibInstaller-Avalonia/ViewModels/FinalPageViewModel.cs
@@ -1,19 +1,53 @@
-﻿using WPILibInstaller.Interfaces;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using WPILibInstaller.Interfaces;
 
 namespace WPILibInstaller.ViewModels
 {
     public class FinalPageViewModel : PageViewModelBase
     {
         private readonly IProgramWindow progWindow;
+        private readonly IConfigurationProvider configurationProvider;
+        private readonly bool vsCodeInstalled;
 
-        public FinalPageViewModel(IProgramWindow progWindow)
+        public string FinishText { get; }
+
+        public FinalPageViewModel(IProgramWindow progWindow, IConfigurationProvider configurationProvider, IToInstallProvider toInstallProvider)
             : base("Finish", "")
         {
+            vsCodeInstalled = toInstallProvider.Model.InstallVsCode;
+            if (vsCodeInstalled)
+            {
+                if (OperatingSystem.IsMacOS())
+                {
+                    FinishText = "Clicking finish will open the VS Code folder. Please drag this to your dock.";
+                }
+                else
+                {
+                    FinishText = "Use the WPILib VS Code desktop icon to start VS Code.";
+                }
+            }
+            else
+            {
+                FinishText = "";
+            }
+
             this.progWindow = progWindow;
+            this.configurationProvider = configurationProvider;
         }
 
         public override PageViewModelBase MoveNext()
         {
+            if (vsCodeInstalled && OperatingSystem.IsMacOS())
+            {
+                ProcessStartInfo pstart = new ProcessStartInfo("open", Path.Join(configurationProvider.InstallDirectory, "vscode"));
+                var p = Process.Start(pstart);
+                if (p != null)
+                {
+                    p.WaitForExit();
+                }
+            }
             progWindow.CloseProgram();
             return this;
         }
