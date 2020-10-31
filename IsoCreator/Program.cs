445f70e6029a31ac22bf1c5a05c3c373a2e4895b
Thad House <thadhouse1@gmail.com>
2020-10-30 23:18:28 -0700

    Fix volume id for iso

--- IsoCreator/Program.cs
+++ IsoCreator/Program.cs
@@ -35,7 +35,7 @@ namespace IsoCreator
 
             CDBuilder builder = new CDBuilder();
             builder.UseJoliet = true;
-            builder.VolumeIdentifier = $"WPILib Installer - {version}";
+            builder.VolumeIdentifier = $"WPILIB_{version.Replace('-', '_').Replace(' ','_')}";
 
             foreach (var file in Directory.EnumerateFiles(input))
             {
