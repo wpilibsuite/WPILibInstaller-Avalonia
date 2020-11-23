80498484d7f62a0b21c20a74e7d9f53ba8a794bc
Joe <sciencewhiz@users.noreply.github.com>
2020-11-23 10:42:25 -0800

    Add error popup when shortcut creation fails
    Depends on #108 so that message is shown instead of exception
    Fixes #84

--- WPILibInstaller-Avalonia/ViewModels/InstallPageViewModel.cs
+++ WPILibInstaller-Avalonia/ViewModels/InstallPageViewModel.cs
@@ -642,7 +642,7 @@ namespace WPILibInstaller.ViewModels
                startInfo.WorkingDirectory = Environment.CurrentDirectory;
                if (shortcutData.IsAdmin)
                {
-                   startInfo.Verb = "runsas";
+                   startInfo.Verb = "runas";
                }
                var exitCode = await Task.Run(() =>
                {
@@ -653,8 +653,10 @@ namespace WPILibInstaller.ViewModels
 
                if (exitCode != 0)
                {
-                   // Print a message saying not all shortcuts were successful
-               }
+                    var result = await MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Warning",
+                   "Shortcut creation failed. Error Code: " + exitCode,
+                   icon: MessageBox.Avalonia.Enums.Icon.Warning, @enum: MessageBox.Avalonia.Enums.ButtonEnum.Ok).ShowDialog(programWindow.Window);
+                }
            }
            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
            {
