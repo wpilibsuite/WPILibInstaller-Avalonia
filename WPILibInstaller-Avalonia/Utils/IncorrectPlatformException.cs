bcbc3fe9faa7eea221ae80b221fa9f8f6315d021
Thad House <thadhouse1@gmail.com>
2020-11-28 20:05:51 -0800

    Verify platform being used for installer.
    This will make sure 32 bit is ran on 32 bit, and 64 bit on 64 bit. Since 32 bit installer will run on 64 bit windows, this is a necessary check

---
+++ WPILibInstaller-Avalonia/Utils/IncorrectPlatformException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPILibInstaller.Utils
+{
+    public class IncorrectPlatformException : Exception
+    {
+
+        public IncorrectPlatformException(string platform) : base($"Installer {platform} needed for current system")
+        {
+            
+        }
+    }
+}
