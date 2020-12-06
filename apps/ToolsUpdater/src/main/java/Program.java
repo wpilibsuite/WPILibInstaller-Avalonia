fa10ac1ad9aadd2dcc4f78d285a50c1c0e7ea55b
Thad House <thadhouse1@gmail.com>
2020-12-05 18:16:41 -0800

    Add support for running C++ apps from the installer tools directory

--- apps/ToolsUpdater/src/main/java/Program.java
+++ apps/ToolsUpdater/src/main/java/Program.java
@@ -7,10 +7,72 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
+import net.lingala.zip4j.ZipFile;
 
 import com.google.gson.Gson;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
+
 public class Program {
+  private static void installJavaTool(ToolConfig tool, String mavenDir, String toolsPath) {
+    ArtifactConfig artifact = tool.artifact;
+    String artifactFileName = artifact.artifactId + '-' + artifact.version;
+    if (artifact.classifier != null && !artifact.classifier.isBlank()) {
+      artifactFileName += '-' + artifact.classifier;
+    }
+    artifactFileName += '.' + artifact.extension;
+
+    Path artifactPath = Paths.get(mavenDir, artifact.groupId.replace('.', File.separatorChar), artifact.artifactId, artifact.version, artifactFileName);
+    if (artifactPath.toFile().exists()) {
+      try {
+        Files.copy(artifactPath, Paths.get(toolsPath, tool.name + ".jar"), StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(Paths.get(toolsPath, "ScriptBase.vbs"), Paths.get(toolsPath, tool.name + ".vbs"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+        Files.copy(Paths.get(toolsPath, "ScriptBase.py"), Paths.get(toolsPath, tool.name + ".py"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+      } catch (IOException e) {
+        System.out.println(e.toString());
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private static String getPlatformPath() {
+    if (SystemUtils.IS_OS_WINDOWS) {
+      return System.getProperty("os.arch").equals("amd64") ? "windows\\x86-64" : "windows\\x86";
+    } else {
+      return SystemUtils.IS_OS_MAC ? "osx/x86-64" : "linux/x86-64";
+    }
+  }
+
+  private static void installCppTool(ToolConfig tool, String mavenDir, String toolsPath) {
+    ArtifactConfig artifact = tool.artifact;
+    String artifactFileName = artifact.artifactId + '-' + artifact.version;
+    artifactFileName += '-' + artifact.classifier;
+    artifactFileName += '.' + artifact.extension;
+
+    Path artifactPath = Paths.get(mavenDir, artifact.groupId.replace('.', File.separatorChar), artifact.artifactId, artifact.version, artifactFileName);
+    if (artifactPath.toFile().exists()) {
+      try {
+
+        File tempDir = new File(toolsPath, "tmp");
+        new ZipFile(artifactPath.toFile()).extractAll(tempDir.getAbsolutePath());
+
+        // Find glass folder
+        File exeFolder = new File(tempDir, getPlatformPath());
+
+        FileUtils.copyDirectory(exeFolder, new File(toolsPath));
+
+        FileUtils.deleteDirectory(tempDir);
+
+        Files.copy(Paths.get(toolsPath, "ScriptBaseCpp.vbs"), Paths.get(toolsPath, tool.name + ".vbs"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+        Files.copy(Paths.get(toolsPath, "ScriptBaseCpp.py"), Paths.get(toolsPath, tool.name + ".py"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+      } catch (IOException e) {
+        System.out.println(e.toString());
+        e.printStackTrace();
+      }
+    }
+  }
+
   public static void main(String[] args) throws URISyntaxException, IOException {
     Gson gson = new Gson();
 
@@ -27,23 +89,10 @@ public class Program {
     try (FileReader reader = new FileReader(jsonFile)) {
       ToolConfig[] tools = gson.fromJson(reader, ToolConfig[].class);
       Arrays.stream(tools).filter(x -> x.isValid()).forEach(tool -> {
-        ArtifactConfig artifact = tool.artifact;
-        String artifactFileName = artifact.artifactId + '-' + artifact.version;
-        if (artifact.classifier != null && !artifact.classifier.isBlank()) {
-          artifactFileName += '-' + artifact.classifier;
-        }
-        artifactFileName += '.' + artifact.extension;
-
-        Path artifactPath = Paths.get(mavenDir, artifact.groupId.replace('.', File.separatorChar), artifact.artifactId, artifact.version, artifactFileName);
-        if (artifactPath.toFile().exists()) {
-          try {
-            Files.copy(artifactPath, Paths.get(toolsPath, tool.name + ".jar"), StandardCopyOption.REPLACE_EXISTING);
-            Files.copy(Paths.get(toolsPath, "ScriptBase.vbs"), Paths.get(toolsPath, tool.name + ".vbs"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
-            Files.copy(Paths.get(toolsPath, "ScriptBase.py"), Paths.get(toolsPath, tool.name + ".py"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
-          } catch (IOException e) {
-            System.out.println(e.toString());
-            e.printStackTrace();
-          }
+        if (tool.cpp) {
+          installCppTool(tool, mavenDir, toolsPath);
+        } else {
+          installJavaTool(tool, mavenDir, toolsPath);
         }
       });
     }
---
+++ files/ScriptBaseCpp.vbs
@@ -0,0 +1,41 @@
+'Create File System Object for working with directories
+Set fso = WScript.CreateObject("Scripting.FileSystemObject")
+
+'Get the script name, and from that the jar name
+exeName = fso.GetBaseName(WScript.ScriptName) + ".exe"
+
+'Get the folder of this script
+toolsFolder = fso.GetParentFolderName(WScript.ScriptFullName)
+
+'Get the full path to the Jar
+fullExeName = fso.BuildPath(toolsFolder, fullExeName)
+
+shellScript = fullExeName
+
+'Create Shell Object
+Set objShell = WScript.CreateObject( "WScript.Shell" )
+dim runObject
+' Allow us to catch a script run failure
+On Error Resume Next
+Set runObj = objShell.Exec(shellScript)
+If Err.Number <> 0 Then
+	If WScript.Arguments.Count > 0 Then
+		If (WScript.Arguments(0) <> "silent") Then
+			WScript.Echo "Error Launching Tool" + vbCrLf + Err.Description
+		Else
+			WScript.StdOut.Write("Error Launching Tool")
+			WScript.StdOut.Write(Error.Description)
+		End If
+	Else
+		WScript.Echo "Error Launching Tool"  + vbCrLf + Err.Description
+	End If
+	Set runObj = Nothing
+	Set objShell = Nothing
+	Set fso = Nothing
+	WScript.Quit(1)
+End If
+
+Set runObj = Nothing
+Set objShell = Nothing
+Set fso = Nothing
+WScript.Quit(0)
