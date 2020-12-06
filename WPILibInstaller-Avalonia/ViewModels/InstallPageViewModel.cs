4a5209ae72e5d29a89acc9e05f3bbf0c247c900d
Daltz333 <daltzsmith@gmail.com>
2020-12-06 01:56:58 -0500

    Missing year reference

--- WPILibInstaller-Avalonia/ViewModels/InstallPageViewModel.cs
+++ WPILibInstaller-Avalonia/ViewModels/InstallPageViewModel.cs
@@ -670,8 +670,8 @@ namespace WPILibInstaller.ViewModels
             shortcutData.StartMenuShortcuts.Add(new ShortcutInfo(Path.Join(frcHomePath, "tools", "SmartDashboard.vbs"), $"{frcYear} WPILib Tools/SmartDashboard", "SmartDashboard"));
 
             // Add Documentation Shortcuts
-            shortcutData.DesktopShortcuts.Add(new ShortcutInfo(Path.Join(frcHomePath, "documentation", "rtd", "frc-docs-latest", "index.html"), "WPILib Documentation", $"WPILib Documentation {frcYear}"));
-            shortcutData.StartMenuShortcuts.Add(new ShortcutInfo(Path.Join(frcHomePath, "documentation", "rtd", "frc-docs-latest", "index.html"), "WPILib Documentation", $"WPILib Documentation {frcYear}"));
+            shortcutData.DesktopShortcuts.Add(new ShortcutInfo(Path.Join(frcHomePath, "documentation", "rtd", "frc-docs-latest", "index.html"), $"WPILib Documentation {frcYear}", $"WPILib Documentation {frcYear}"));
+            shortcutData.StartMenuShortcuts.Add(new ShortcutInfo(Path.Join(frcHomePath, "documentation", "rtd", "frc-docs-latest", "index.html"), $"WPILib Documentation {frcYear}", $"WPILib Documentation {frcYear}"));
 
 
             var serializedData = JsonConvert.SerializeObject(shortcutData);
