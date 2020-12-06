49284d1d90af1950992870ccc2bdd5bccab2dde0
Dalton Smith <darasmith@wccnet.edu>
2020-12-06 10:30:52 -0500

    Update InstallPageViewModel.cs

--- WPILibInstaller-Avalonia/ViewModels/InstallPageViewModel.cs
+++ WPILibInstaller-Avalonia/ViewModels/InstallPageViewModel.cs
@@ -653,7 +653,7 @@ namespace WPILibInstaller.ViewModels
             shortcutData.IsAdmin = toInstallProvider.Model.InstallAsAdmin;
 
             // Add VS Code Shortcuts
-            shortcutData.DesktopShortcuts.Add(new ShortcutInfo(Path.Join(frcHomePath, "vscode", "Code.exe"), $"WPILib VS Code {frcYear}", $"FRC VS Code {frcYear}"));
+            shortcutData.DesktopShortcuts.Add(new ShortcutInfo(Path.Join(frcHomePath, "vscode", "Code.exe"), $"WPILib VS Code {frcYear}", $"WPILib VS Code {frcYear}"));
             shortcutData.StartMenuShortcuts.Add(new ShortcutInfo(Path.Join(frcHomePath, "vscode", "Code.exe"), $"WPILib VS Code {frcYear}", $"WPILib VS Code {frcYear}"));
 
             // Add Tool Shortcuts
