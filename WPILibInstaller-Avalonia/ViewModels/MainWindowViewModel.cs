cb68b415b7ef231cff2d2813fb40180f8257185f
Thad House <ThadHouse@users.noreply.github.com>
2021-12-19 23:08:40 -0800

    Run dotnet format using dotnet 6 sdk (#226)
    The separate tool is no longer required, and results in a different format then the sdk directly

--- WPILibInstaller-Avalonia/ViewModels/MainWindowViewModel.cs
+++ WPILibInstaller-Avalonia/ViewModels/MainWindowViewModel.cs
@@ -20,7 +20,7 @@ namespace WPILibInstaller.ViewModels
             }
         }
 
-        private readonly Stack<PageViewModelBase> pages = new ();
+        private readonly Stack<PageViewModelBase> pages = new();
 
         public string? ForwardName => CurrentPage?.ForwardName;
 
