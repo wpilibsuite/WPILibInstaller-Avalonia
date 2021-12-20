45228437b6e48d50e78b5ac265b07c7996d11376
Thad House <ThadHouse@users.noreply.github.com>
2021-12-19 16:44:18 -0800

    Abort install if a JDK process to be installed over is running (#219)
    * Abort install if a JDK process to be installed over is running
    
    * Formatting

--- WPILibInstaller-Avalonia/ViewModels/InstallPageViewModel.cs
+++ WPILibInstaller-Avalonia/ViewModels/InstallPageViewModel.cs
@@ -436,6 +436,61 @@ namespace WPILibInstaller.ViewModels
         private async Task ExtractArchive(CancellationToken token, string[]? filter)
         {
             Progress = 0;
+            if (OperatingSystem.IsWindows())
+            {
+                Text = "Checking for currently running JDKs";
+                bool foundRunningExe = await Task.Run(() =>
+                {
+                    try
+                    {
+                        var jdkBinFolder = Path.Join(configurationProvider.InstallDirectory, configurationProvider.JdkConfig.Folder, "bin");
+                        var jdkExes = Directory.EnumerateFiles(jdkBinFolder, "*.exe", SearchOption.AllDirectories);
+                        bool found = false;
+                        foreach (var exe in jdkExes)
+                        {
+                            try
+                            {
+                                var name = Path.GetFileNameWithoutExtension(exe)!;
+                                var pNames = Process.GetProcessesByName(name);
+                                foreach (var p in pNames)
+                                {
+                                    if (p.MainModule?.FileName == exe)
+                                    {
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                                if (found)
+                                {
+                                    break;
+                                }
+                            }
+                            catch
+                            {
+                                // Do nothing. We don't want this code to break.
+                            }
+                        }
+                        return found;
+                    }
+                    catch
+                    {
+                        // Do nothing. We don't want this code to break.
+                        return false;
+                    }
+                });
+                if (foundRunningExe)
+                {
+                    string msg = "Running JDK processes have been found. Installation cannot continue. Please restart your computer, and rerun this installer without running thing else (including VS Code)";
+                    await MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow(new MessageBox.Avalonia.DTO.MessageBoxStandardParams
+                    {
+                        ContentTitle = "JDKs Running",
+                        ContentMessage = msg,
+                        Icon = MessageBox.Avalonia.Enums.Icon.Error,
+                        ButtonDefinitions = MessageBox.Avalonia.Enums.ButtonEnum.Ok
+                    }).ShowDialog(programWindow.Window);
+                    throw new InvalidOperationException(msg);
+                }
+            }
 
             var archive = configurationProvider.ZipArchive;
 
