f365a2ee9c42bced300e65dc142cdb4501a2ef4a
sciencewhiz <sciencewhiz@users.noreply.github.com>
2024-09-29 16:14:48 -0700

    Fix macOS path

--- files/AdvantageScope.sh
+++ files/AdvantageScope.sh
@@ -17,7 +17,7 @@ unset ELECTRON_RUN_AS_NODE
 if [ "$OS_NAME" = "Linux" ]; then
     exec "$AS_PATH/advantagescope-wpilib"
 elif [ "$OS_NAME" = "Darwin" ]; then
-    open "AdvantageScope (WPILib).app"
+    open "$AS_PATH/AdvantageScope (WPILib).app"
 else
     exec "$AS_PATH/advantagescope-wpilib"
 fi
