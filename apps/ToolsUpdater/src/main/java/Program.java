070b8a2d856be95548ee11b5e4caf3730c59fe52
sciencewhiz <sciencewhiz@users.noreply.github.com>
2024-09-29 17:22:29 -0700

    Add shell scripts for launching tools (#420)
    Does not remove the python scripts, but that can be done after successful beta testing.

--- apps/ToolsUpdater/src/main/java/Program.java
+++ apps/ToolsUpdater/src/main/java/Program.java
@@ -32,6 +32,7 @@ public class Program {
           Files.copy(Paths.get(toolsPath, "ScriptBase.vbs"), Paths.get(toolsPath, tool.name + ".vbs"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
         } else {
           Files.copy(Paths.get(toolsPath, "ScriptBase.py"), Paths.get(toolsPath, tool.name + ".py"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+          Files.copy(Paths.get(toolsPath, "ScriptBase.sh"), Paths.get(toolsPath, tool.name + ".sh"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
         }
       } catch (IOException e) {
         System.out.println(e.toString());
@@ -86,7 +87,9 @@ public class Program {
           Files.copy(Paths.get(toolsPath, "ScriptBaseCpp.vbs"), Paths.get(toolsPath, tool.name + ".vbs"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
         } else {
           Files.copy(Paths.get(toolsPath, "ScriptBaseCpp.py"), Paths.get(toolsPath, tool.name + ".py"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+          Files.copy(Paths.get(toolsPath, "ScriptBaseCpp.sh"), Paths.get(toolsPath, tool.name + ".sh"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
         }
+
       } catch (IOException e) {
         System.out.println(e.toString());
         e.printStackTrace();
