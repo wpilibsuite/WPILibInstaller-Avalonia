2a2e7d0db99cfa041350c928a08c30c42a282dc1
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-11-14 15:46:57 -0500

    Fix elastic zip extraction

--- apps/ToolsUpdater/src/main/java/Program.java
+++ apps/ToolsUpdater/src/main/java/Program.java
@@ -116,6 +116,23 @@ public class Program {
     }
   }
 
+  private static void installElastic(String toolsPath) {
+    if (SystemUtils.IS_OS_MAC) {
+      String archiveFileName = "Elastic-WPILib-macOS.tar.gz";
+      String elasticFolder = Paths.get(new File(toolsPath).getParent(), "elastic").toString();
+      Path archivePath = Paths.get(elasticFolder, archiveFileName);
+
+      try {
+        Runtime.getRuntime().exec(new String[] {
+          "tar", "-xzf", archivePath.toString(), "-C", elasticFolder
+        }).waitFor();
+      } catch (IOException | InterruptedException e) {
+        System.out.println(e.toString());
+        e.printStackTrace();
+      }
+    }
+  }
+
   public static void main(String[] args) throws URISyntaxException, IOException {
     Gson gson = new Gson();
 
@@ -131,6 +148,8 @@ public class Program {
         System.out.println("Installing " + tool.name);
         if (tool.name.equals("AdvantageScope")) {
           installAdvantageScope(toolsPath);
+        } else if (tool.name.equals("Elastic")) {
+          installElastic(toolsPath);
         } else if (tool.artifact != null) {
           if (tool.cpp) {
             installCppTool(tool, toolsPath);
