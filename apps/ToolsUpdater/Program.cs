c8bfe30da4711d426eaaaefb1a425766251c62c9
project516 <138796702+Project516@users.noreply.github.com>
2026-05-01 11:01:12 -0500

    Update Program.cs
    Add using System.Diagnostics

--- apps/ToolsUpdater/Program.cs
+++ apps/ToolsUpdater/Program.cs
@@ -2,6 +2,7 @@
 using System.IO.Compression;
 using System.Runtime.InteropServices;
 using System.Text.Json;
+using System.Diagnostics;
 using ToolsUpdater;
 
 async Task InstallElastic(string toolsPath)
